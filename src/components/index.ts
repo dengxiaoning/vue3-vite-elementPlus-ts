b5eea162bf5663f77db6968386cbc0d539e3fadf
dex <412693453@qq.com>
2021-10-20 22:17:17 +0800

    增加cesium

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,25 +1,13 @@
 
-// import type { App } from 'vue'
-
-// export function setupGlobalCom(app:App<Element>) {
-//   // Dynamic injection modules
-//   const modulesGlob = import.meta.globEager('./*/index.*');
-//   Object.keys(modulesGlob).map((key) => {
-//     // 使用文件夹名称作为comp key
-//     const compKey = key.split('/')[1];
-//     app.component(compKey, modulesGlob[key].default);
-// });
-
-// }
-
 import type { App } from 'vue'
-import SvgIcon from './SvgIcon/index.vue'
-import Iconify from './CIcon/index.vue'
-
 
 export function setupGlobalCom(app:App<Element>) {
-
-  app.component('SvgIcon', SvgIcon)
-  app.component('CIcon', Iconify)
+  // Dynamic injection modules
+  const modulesGlob = import.meta.globEager('./*/index.*');
+  Object.keys(modulesGlob).map((key) => {
+    // 使用文件夹名称作为comp key
+    const compKey = key.split('/')[1];
+    app.component(compKey, modulesGlob[key].default);
+});
 
 }
